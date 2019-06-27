dc4a25b2fd8a2fb39dbf71ce559c262a6ac17e64
github username <github email address>
2019-06-26 23:39:38 -0400

    refactored turn functionality

--- ttt.js
+++ ttt.js
@@ -1,36 +1,37 @@
-// when a square is clicked, the innerText should becomse an 'X' or an 'O'
-document.querySelector(".squares").addEventListener("click", e => {
-  buttonClick(e.target);
-});
-
-// change innerText to 'X', 'O', or '-'
-const buttonClick = val => {
-  if (currentTurn.innerText === "Player O") {
-    val.innerText = "O";
-  } else if (currentTurn.innerText === "Player X") {
-    val.innerText = "X";
-  } else {
-    val.innerText = "-";
-  }
-};
-
 // game starts with Player X
-// when player clicks an empty square it changes to their token
-// need to track current player
 
 // current turn
 const currentTurn = document.querySelector(".current-turn");
 
-const turnBtn = document
-  .querySelector(".turn-btn")
-  .addEventListener("click", e => {
-    changePlayer(currentTurn);
-  });
+// when a square is clicked, the innerText should becomse an 'X' or an 'O'
+// add event delegation to squares
+document.querySelector(".squares").addEventListener("click", e => {
+  squareClick(e.target);
+});
 
-const changePlayer = val => {
-  if (val.innerText === "Player X") {
-    currentTurn.innerText = "Player O";
-  } else {
-    currentTurn.innerText = "Player X";
+// when player clicks an empty square it changes to their token
+const squareClick = val => {
+  if (val.innerText === "-") {
+    val.innerText = currentTurn.innerText === "Player O" ? "O" : "X";
+    changePlayer(currentTurn);
   }
 };
+
+const changePlayer = val => {
+  val.innerText === "Player X"
+    ? (currentTurn.innerText = "Player O")
+    : (currentTurn.innerText = "Player X");
+};
+
+// reset game
+document.querySelector(".reset-btn").addEventListener("click", e => {
+  clearBoard();
+});
+
+const clearBoard = () => {
+  const board = document.querySelectorAll(".square");
+  board.forEach(square => {
+    square.innerHTML = "-";
+  });
+};
+
